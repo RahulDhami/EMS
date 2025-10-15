6037016ba3f043ed472262f167049f89c5f0de0f
rahul <rahuldhami11221@gmail.com>
2025-10-15 10:27:39 +0530

    admin dashboard built

---
+++ frontend/src/components/AdminSidebar.jsx
@@ -0,0 +1,63 @@
+import React from 'react'
+import {NavLink} from 'react-router-dom'
+import {FaTachometerAlt,FaUsers,FaBuilding} from 'react-icons/fa'
+const AdminSidebar = () => {
+  return (
+    <div className='bg-gray-800 text-white h-screen fixed left-0 top-0 bottom-0 space-y-8 w-64'>
+      <div className='text-2xl text-center font-pacific'>
+        Employee Ms
+      </div>
+      <div className='px-4' >
+        <NavLink to="/admin-dashboard"
+        className={({isActive})=>`${isActive?"bg-teal-500":""} flex items-center space-x-4 py-2.5 px-4 rounded`}>
+<FaTachometerAlt/>
+<span>
+    Dashboard
+</span>
+        </NavLink>
+           <NavLink to="/"
+           className='flex items-center space-x-4 py-2.5 px-4 rounded'>
+<FaUsers/>
+<span>
+    Employee
+</span>
+        </NavLink>
+            
+           <NavLink to="/"
+           className='flex items-center space-x-4 py-2.5 px-4 rounded'>
+<FaBuilding/>
+<span>
+    Employee
+</span>
+        </NavLink>
+
+                <NavLink to="/"
+                className='flex items-center space-x-4 py-2.5 px-4 rounded'>
+<FaBuilding/>
+<span>
+    Employee
+</span>
+        </NavLink>
+
+
+                <NavLink to="/"
+                className='flex items-center space-x-4 py-2.5 px-4 rounded'>
+<FaBuilding/>
+<span>
+    Employee
+</span>
+        </NavLink>
+
+                <NavLink to="/"
+                className='flex items-center space-x-4 py-2.5 px-4 rounded'>
+<FaBuilding/>
+<span>
+    Employee
+</span>
+        </NavLink>
+      </div>
+    </div>
+  )
+}
+
+export default AdminSidebar
--- frontend/src/pages/AdminDashboard.jsx
+++ frontend/src/pages/AdminDashboard.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import {useAuth} from '../context/authContext'
-
+import AdminSidebar from '../components/AdminSidebar'
+import Navbar from '../components/Navbar'
 const AdminDashboard = () => {
   const {user }=useAuth()
 
   return (
-    <div>
-    AdminDashboard — Welcome {user?.name || "Admin"}
+    <div className='flex'>
+  <AdminSidebar/> 
+  <div className='flex-1 ml-64'>
+<Navbar/>
+  </div>
     </div>
   )
 }
